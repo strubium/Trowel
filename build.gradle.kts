b8d05921dcdfa09775903ce594658044cebda711
strubium <113206902+strubium@users.noreply.github.com>
2025-07-23 10:51:41 -0500

    1.16.5 port part one

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("fabric-loom") version "1.6.11" // or latest
+    id("fabric-loom") version "1.6.11"
 }
 
 group = "com.theendercore"
@@ -15,10 +15,10 @@ repositories {
 }
 
 dependencies {
-    minecraft("com.mojang:minecraft:1.19.2")
-    mappings("net.fabricmc:yarn:1.19.2+build.28:v2")
-    modImplementation("net.fabricmc:fabric-loader:0.15.10")
-    modImplementation("net.fabricmc.fabric-api:fabric-api:0.77.0+1.19.2")
+    minecraft("com.mojang:minecraft:1.16.5")
+    mappings("net.fabricmc:yarn:1.16.5+build.9:v2")
+    modImplementation("net.fabricmc:fabric-loader:0.11.6")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:0.42.0+1.16")
 }
 
 loom {
@@ -30,14 +30,12 @@ loom {
     }
 }
 
-tasks {
-    withType<JavaCompile> {
-        options.encoding = "UTF-8"
-        options.release.set(21)
-    }
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(8)) // Java 8 is standard for 1.16.5
+    withSourcesJar()
+}
 
-    java {
-        toolchain.languageVersion.set(JavaLanguageVersion.of(21))
-        withSourcesJar()
-    }
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+    // No `options.release.set(21)` since we're targeting Java 8
 }
--- src/main/java/com/theendercore/trowel/TrowelMod.java
+++ src/main/java/com/theendercore/trowel/TrowelMod.java
@@ -7,23 +7,19 @@ import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class TrowelMod implements ModInitializer {
     public static final String MODID = "trowel";
-    public static final Logger LOG = LoggerFactory.getLogger(MODID);
     public static final Item TROWEL = new Trowel();
 
     public static final ItemGroup TROWEL_GROUP = FabricItemGroupBuilder.create(
                     new Identifier(MODID, "trowel_group"))
             .icon(() -> new ItemStack(TROWEL))
-            .appendItems(stacks -> stacks.add(new ItemStack(TROWEL)))
             .build();
 
     @Override
     public void onInitialize() {
-        LOG.info("Seizing the means of block placement!");
+        System.out.println("Seizing the means of block placement!");
         Registry.register(Registry.ITEM, new Identifier(MODID, MODID), TROWEL);
     }
 }
